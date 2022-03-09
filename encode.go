5d1dcc421cb558d67599d063dff275d0ab1d1d5a
xuwf <xuwenfa@star-net.cn>
2022-03-09 16:50:01 +0800

    fix: support Md5File

--- encode.go
+++ encode.go
@@ -5,31 +5,37 @@ import (
     "fmt"
     "io"
     "crypto/md5"
-    "encoding/hex"
 )
 
 func Md5(content string) (string, error) {
     h := md5.New()
     h.Write([]byte(content))
-    return hex.EncodeToString(h.Sum(nil)), nil
+
+    //将 []byte 转为 string
+    md5str := fmt.Sprintf("%x", h.Sum(nil))
+
+    return md5str, nil
 }
 
 func Md5File(filename string) (string, error) {
-    f, err := os.Open(filename) //打开文件
+    //打开文件
+    f, err := os.Open(filename)
     if nil != err {
         fmt.Println(err)
         return "", err
     }
     defer f.Close()
 
-    md5Handle := md5.New()      //创建 md5 句柄
-    _, err = io.Copy(md5Handle, f)  //将文件内容拷贝到 md5 句柄中
+    //计算 MD5 值，返回 []byte
+    md5Handle := md5.New()
+    _, err = io.Copy(md5Handle, f)
     if nil != err {
         fmt.Println(err)
         return "", err
     }
-    md := md5Handle.Sum(nil)    //计算 MD5 值，返回 []byte
+    md := md5Handle.Sum(nil)
 
-    md5str := fmt.Sprintf("%x", md) //将 []byte 转为 string
+    //将 []byte 转为 string
+    md5str := fmt.Sprintf("%x", md)
     return md5str, nil
 }
