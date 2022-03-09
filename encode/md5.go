772988b080ce2d91eba564126c2a96f5477a7442
xuwf <xuwenfa@star-net.cn>
2022-03-09 13:45:32 +0800

    feat: add md5

---
+++ encode/md5.go
@@ -0,0 +1,12 @@
+package md5
+
+import (
+    "crypto/md5"
+    "encoding/hex"
+)
+
+func Md5(content string)(string) {
+    h := md5.New()
+    h.Write([]byte(content))
+    return hex.EncodeToString(h.Sum(nil))
+}
